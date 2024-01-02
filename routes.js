4a1d5998befa5bccee4ddeae82e7bc89720b8737
Dolraith <Dolraith@Gmail.com>
2024-01-01 19:40:20 -0500

    new routes

--- routes.js
+++ routes.js
@@ -2,3 +2,7 @@
 
 global._router.setRoutes(['spirits'], 'modules/Miko/_controllers/c_spirits');
 global._router.setRoutes(['spirit_admin'], 'modules/Miko/_controllers/c_spirit_admin');
+
+/* Navbar config */
+global._router.setNav("Miko", "/spirits", "Spirit", null);
+global._router.setNav("Miko", "/spirit_admin", "Spirit Admin", "miko_admin");
